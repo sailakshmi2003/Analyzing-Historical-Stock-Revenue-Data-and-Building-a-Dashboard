3f6ce7c90dc26cbc557977f53edfdf173dd740f7
sailakshmi2003 <83651850+sailakshmi2003@users.noreply.github.com>
2022-11-23 15:00:37 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import yfinance as yf
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+tesla = yf.Ticker("TSLA")
+tesla_data = tesla.history(period="max")
+tesla_data.reset_index(inplace=True)
+tesla_data.head()
+
+url = "https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue"
+html_data = requests.get(url).text
+soup = BeautifulSoup(html_data, "html.parser")
+soup.find_all('title')
+
+tesla_revenue = pd.DataFrame(columns = ['Date', 'Revenue'])
+
+for row in soup.find_all("tbody")[1].find_all("tr"):
+    col = row.find_all("td")
+    date = col[0].text
+    revenue = col[1].text.replace("$", "").replace(",", "")
+   
+    tesla_revenue = tesla_revenue.append({"Date": date, "Revenue": revenue}, ignore_index = True)
+tesla_revenue.dropna(inplace=True)
+tesla_revenue = tesla_revenue[tesla_revenue['Revenue'] != ""]
+tesla_revenue.tail()
+gamestop = yf.Ticker("GME")
+gamestop_data = gamestop.history(period="max")
+gamestop_data.reset_index(inplace=True)
+gamestop_data.head()
+
+url1 = "https://www.macrotrends.net/stocks/charts/GME/gamestop/revenue"
+html_data = requests.get(url1).text
+soup = BeautifulSoup(html_data, "html.parser")
+soup.find_all('title')
+
+gme_revenue = pd.DataFrame(columns=['Date', 'Revenue'])
+
+for table in soup.find_all('table'):
+
+    if ('GameStop Quarterly Revenue' in table.find('th').text):
+        rows = table.find_all('tr')
+       
+        for row in rows:
+            col = row.find_all('td')
+           
+            if col != []:
+                date = col[0].text
+                revenue = col[1].text.replace(',','').replace('$','')
+
+                gme_revenue = gme_revenue.append({"Date":date, "Revenue":revenue}, ignore_index=True)
+gme_revenue.tail()
+
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data.Date, infer_datetime_format=True), y=stock_data.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data.Date, infer_datetime_format=True), y=revenue_data.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+make_graph(tesla_data, tesla_revenue, 'Tesla')
